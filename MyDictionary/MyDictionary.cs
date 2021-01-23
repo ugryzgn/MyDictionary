1e5646264590f8ed20bc7d3d61420bc352f1dd9f
Uğur Yazgan <ugryazgn@gmail.com>
2021-01-23 20:42:55 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<T>
+    {
+        T[] items;
+        public Dictionary()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string> vocabularies = new Dictionary<string>();
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            vocabularies.Add("Pencil");
+            Console.WriteLine("Kelime Sayısı: " + vocabularies.Count);
+
+        }
+    }
+}
